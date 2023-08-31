81ba45a9bbb45d82c13f4cb6f038aface93dc9d2
hara0510 <oqj0510@gmail.com>
2023-08-31 22:35:04 +0900

    編集・削除ボタンの表示設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,16 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
   private
 
   def prototype_params
