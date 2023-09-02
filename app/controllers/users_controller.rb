283fff8e2bb3628af5c2f52350453d3ddbebf60f
hara0510 <oqj0510@gmail.com>
2023-09-02 18:03:05 +0900

    ユーザー詳細ページの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ def create
 end
 
 def show
+   @user = User.find(params[:id])
 end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:email, :name, :profile, :occupation, :posting)
+end
+
 
 end
